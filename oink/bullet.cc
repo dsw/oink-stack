3e9a7f32984b8c68f64214681363909b12fd1c3f
Derrick Coetzee <dcoetzee@dcoetzee-virtual-ubuntu.(none)>
2009-10-05 18:28:57 -0700

    Woops, forgot a #include

--- oink/bullet.cc
+++ oink/bullet.cc
@@ -23,8 +23,8 @@
 #include <llvm/Analysis/Verifier.h>
 #include <llvm/Assembly/PrintModulePass.h>
 #include <llvm/Support/IRBuilder.h>
-
 #include <llvm/Pass.h>
+#include <llvm/Support/raw_ostream.h>
 
 // Emit stage ****
 
