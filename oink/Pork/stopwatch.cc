fc50b43654f9f1182f8b99e9195f9883a14b6cca
Daniel S. Wilkerson <daniel.wilkerson@gmail.com>
2009-10-13 18:50:59 -0700

    did a detailed cleanup pass organizing the code more clearly and annotating how it works

--- oink/Pork/stopwatch.cc
+++ oink/Pork/stopwatch.cc
@@ -26,70 +26,82 @@ public:
 
   virtual bool visitFunction(Function *func) {
     Variable *v = func->nameAndParams->var;
-    if (v->isImplicitMemberFunc())
-      return false;
-
     std::string fullname = v->fullyQualifiedName0().c_str();
-    if (methods.find(fullname) != methods.end()) {
-      return false;
-    }
     S_compound *body = func->body->asS_compound();
-    // punt on headers
     char const *file = sourceLocManager->getFile(body->loc);
-    if (!strstr(file, ".cpp"))
-      return false;
-    
     CPPSourceLoc csl(body->loc);
 
-    if (!csl.hasExactPosition())
-      return false;
-  
-    std::string indent = "\n";
+    // skip the methods the user specified to skip on the
+    // command-line
+    if (methods.find(fullname) != methods.end()) return false;
 
-    if (body->stmts.isEmpty())
-      return false;
+    // skip implicit members, such as the ctors you get if you don't
+    // write one yourself
+    if (v->isImplicitMemberFunc()) return false;
 
+    // skip bodies where I can't get the exact position (?)
+    if (!csl.hasExactPosition()) return false;
+
+    // skip functions defined in header files; FIX: don't use .cpp
+    if (!strstr(file, ".cpp")) return false;
+
+    // skip functions having empty bodies
+    if (body->stmts.isEmpty()) return false;
     Statement *firstStatement = body->stmts.first();
-
-    // functions that begin with a return usually aren't interesting
-    if (firstStatement->isS_return())
-      return false;
-
     CPPSourceLoc endcsl(firstStatement->loc);
+  
+    // skip functions that amount to a single expression
+    if (firstStatement->isS_return()) return false;
+
+    // FIX: what does this do?  is this all just to compute the
+    // indentation?
+    std::string indent = "\n";
     if (!endcsl.hasExactPosition()) {
-      if (endcsl.macroExpansion)
+      if (endcsl.macroExpansion) {
         endcsl.overrideLoc(endcsl.macroExpansion->preStartLoc);
+      }
       indent = patcher.getRange(PairLoc(csl, endcsl));
       //get rid of {
-      if (indent.size())
+      if (indent.size()) {
         indent.erase(0,1);
+      }
       std::string::size_type endline = indent.rfind("\n");
       if (endline != std::string::npos) {
         indent.erase(0, endline);
       }
     }
 
+    // insert code at the top of the function
     std::stringstream ss;
-    ss << indent << "InstrumentBlock __ib(\"" << fullname
-       << "\");";
+    ss << indent << "InstrumentBlock __ib(\"" << fullname << "\");";
     patcher.insertBefore(csl, ss.str(), 1);
+
+    // idempotently ensure that the top of the file includes the right
+    // header
     ensureInclude(file);
+
+    // prune traversal of the children of this AST node
     return false;
   }
-  
+
+  // insert an #include at the top of the file
   void ensureInclude(char const *file) {
-    if (files.find(file) != files.end())
-      return;
+    // idempotency: don't do this twice for the same file
+    if (files.find(file) != files.end()) return;
     files.insert(file);
+
+    // do the insertion
     std::ifstream f(file);
     std::string linestr;
     int line = 0;
     while (getline(f, linestr)) {
       ++line;
+      // if the first line is an #include
       if (!strncmp("#include", linestr.c_str(), 8)) {
+        // insert code before that line
         UnboxedLoc loc(line, 1);
         patcher.insertBefore(file, loc, "#include \"instrument.h\"\n");
-        break;
+        break;                  // break out of the loop
       }
     }
     f.close();
@@ -110,13 +122,15 @@ int main(int argc, char **argv) {
     ++curarg;
   }
 
+  // parse and run the stopwatch visitor across each translation unit
   PigletParser parser;
   Patcher p;
   Stopwatch visitor(p);
-  for (int i=curarg; i<argc; i++) {
+  for (int i=curarg; i<argc; ++i) {
     TranslationUnit *unit = parser.getASTNoExc(argv[i]);
     unit->traverse(visitor);
   }
 
+  // return success
   return 0;
 }
