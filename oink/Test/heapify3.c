07245f0fa071b2871fa679456f149bf47cc1b0cb
Daniel S. Wilkerson <daniel.wilkerson@gmail.com>
2009-11-03 22:16:05 -0800

    -

--- oink/Test/heapify3.c
+++ oink/Test/heapify3.c
@@ -3,11 +3,19 @@
 void f(int a[]) {
 }
 
-int main() {
+void bar() {
   // from Derrick
   int a[] = { 0, 1, 2 };
+  f(a);
+}
+
+void foo() {
+  // from Derrick
   char str[] = "Hello world!";
   printf(str);
-  f(a);
-  return 0;
+}
+
+void g() {
+  int a2[5] = {0};
+  f(a2);
 }
